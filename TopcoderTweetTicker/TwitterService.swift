3661eb9dc5e203fb33ad63007bd2be3aa451b7a7
David Conner <dconner.pro@gmail.com>
2015-12-15 17:39:02 -0500

    stubbed the definition for TwitterService

---
+++ TopcoderTweetTicker/TwitterService.swift
@@ -0,0 +1,15 @@
+//
+//  TwitterService.swift
+//  TopcoderTweetTicker
+//
+//  Created by David Conner on 12/15/15.
+//  Copyright © 2015 dcunit3d. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class TwitterService {
+    
+}
